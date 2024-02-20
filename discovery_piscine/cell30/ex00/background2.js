034fc6b1aa2ee83914b6bcbee6036bf18830ea64
Julien Ducharlet <laliguedesprofs.mathssciences@gmail.com>
2024-02-20 09:56:27 +0100

    31 OK

--- discovery_piscine/cell30/ex00/background2.js
+++ discovery_piscine/cell30/ex00/background2.js
@@ -2,14 +2,19 @@ function randomColorChannel() {
 	return Math.floor(Math.random() * 256);
 }
 
-function randomRGB() {
+function randomAlphaChannel() {
+	return Math.round(Math.random() * 100) / 100;
+}
+
+function randomRGBA() {
 	let R = randomColorChannel();
 	let G = randomColorChannel();	
 	let B = randomColorChannel();			
+	let A = randomAlphaChannel();			
 	
-	return `rgb(${R}, ${G}, ${B})`;
+	return `rgba(${R}, ${G}, ${B}, ${A})`;
 }
 
 document.getElementById("random-bg-btn").addEventListener("click", function() {
-	document.body.style.backgroundColor = randomRGB();
+	document.body.style.backgroundColor = randomRGBA();
 });
